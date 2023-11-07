140f7385f7139d1b72ea6eb34c9688aeb3cb4c10
Jamil kawsher <jamilkawsher@gmail.com>
2023-11-07 23:57:13 +0600

    basic Struture added

---
+++ test/specs/addNote.js
@@ -0,0 +1,20 @@
+describe("Add Note", () => {
+
+
+    it("Allowing Perssion and Add Note Title And Description", async () => {
+        // Allowing Permiison
+
+
+        //Skiping Welcome Screen
+
+        await $("//android.widget.Button[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip']").click()
+
+
+        //
+        browser.pause(4000)
+
+
+
+
+    })
+})
